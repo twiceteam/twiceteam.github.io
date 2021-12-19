4599aedee22438b4f31961d07ebc2ef80caf2096
Pouria <pooriyaafshari81@gmail.com>
2021-12-19 22:38:48 +0100

    Recovery

--- compiler/ide.js
+++ compiler/ide.js
@@ -3,9 +3,10 @@ window.onload = function(){
     editor = ace.edit("editor");
     editor.setTheme("ace/theme/monokai");
     editor.session.setMode("ace/mode/c_cpp");
+    
 }
 
-document.getElementById("myfile").addEventListener("change", function() {
+document.getElementById("input").addEventListener("change", function() {
     var media = URL.createObjectURL(this.files[0]);
     var video = document.getElementById("video");
     video.src = media;
@@ -17,8 +18,9 @@ document.getElementById("myfile").addEventListener("change", function() {
 
 document.getElementById("mojtaba").addEventListener("click", displayDate);
 function displayDate() {
-  document.getElementsByClassName("myoutput")[1].innerHTML = 'Waiting...';
-  document.getElementsByClassName("myoutput")[0].innerHTML = 'Waiting...';
+ 
+  console.log(editor.getValue());
+
 }
 
 window.onbeforeunload = function (e) {
@@ -40,29 +42,27 @@ function changeLanguage(){
     else if(language == 'php')editor.session.setMode("ace/mode/php");
     else if(language == 'python')editor.session.setMode("ace/mode/python");
     else if(language == 'node')editor.session.setMode("ace/mode/javascript");
+
+
+$.ajax({
+
+    url: "/ide/app/compiler.php",
+
+    method: "POST",
+
+    data:{
+        language: $("#languages").val(),
+        code:editor.getsession().getValue()
+    },
+
+    success: function(response){
+        $(".output").text(response)
+    }
+
+})
+
+
 }
-
-$(document).ready(function () {
-        
-    $('#mojtaba').click(function(){
-      
-      $.ajax('https://pooriya007.pythonanywhere.com/', {
-        type: 'POST',  // http method
-        data: {         code: editor.getValue(),
-                        lang: document.getElementById("languages").value,
-                        inpt: document.getElementsByClassName("myinput")[0].value + document.getElementsByClassName("myinput")[1].value},  // data to submit
-        success: function (data, status, xhr) {
-          document.getElementsByClassName("myoutput")[0].innerHTML = xhr.responseText;
-          document.getElementsByClassName("myoutput")[1].innerHTML = xhr.responseText;
-          
-
-        },
-        error: function (jqXhr, textStatus, errorMessage) {
-                    document.getElementsByClassName("myoutput")[0,1].value = "SERVER ERROR";
-        }
-      });
-    });
-    });
 
 
 
